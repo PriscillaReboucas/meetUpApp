fd2aec2f59a392bdbf86847f1208a70125674475
Priscilla Rebouças <priscillabreboucas@gmail.com>
2022-04-01 20:31:28 +0200

    pokemon

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,45 @@
-import { Route, Routes } from "react-router-dom";
-import AllMeetupsPage from "./pages/AllMeetups";
-import NewMeetupPage from "./pages/NewMeetup";
-import FavoritesPage from "./pages/Favorites";
-import Layout from "./components/layout/Layout";
+import CardList from "./components/CardList";
+import CardContainer from "./components/CardContainer";
+import { useState, useEffect } from "react";
+import pic from './pokedex.png'
+import './App.css'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedPokemons, setLoadedPokemons] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setIsLoading(false);
+        setLoadedPokemons(data.results);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
 
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<AllMeetupsPage />} />
-        <Route path="/new-meetup" element={<NewMeetupPage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-      </Routes>
-    </Layout>
+    <div>
+      <h1 className="title">
+        <img src={pic}></img>
+        <span>Who is this Pokemón?</span>
+      </h1>
+
+      <CardContainer>
+        <CardList pokemons={loadedPokemons} />
+      </CardContainer>
+    </div>
   );
 }
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
-import {FavoritesContextProvider} from './store/favorites-context'
+
 
 
 ReactDOM.render(
-    <FavoritesContextProvider>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </FavoritesContextProvider>
-  , 
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, 
     document.getElementById('root')
 );
 
